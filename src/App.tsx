873e5d66a562c523420448c6a6cdf521bf5a5703
Sumon Chandra <topu199041@gmail.com>
2023-09-16 10:37:31 +0600

    Initial Project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+     return (
+          <main className="w-full min-h-screen px-5 mx-auto lg:px-0 lg:w-5/12">
+               <div>
+                    <h3>Hello World</h3>
+               </div>
+          </main>
+     );
+}
+
+export default App;
