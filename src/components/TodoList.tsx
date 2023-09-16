b2c39658464f37b4dca524b306c619fab55b17c1
Sumon Chandra <topu199041@gmail.com>
2023-09-16 21:31:39 +0600

    Handle Edit todo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,24 +1,26 @@
-import { useState, useEffect, useRef, SyntheticEvent } from "react";
+import { useState, useEffect, useRef } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import { Todo } from "../types";
 import axios from "axios";
 import removeImg from "../assets/remove.png";
+import editImg from "../assets/edit.png";
 
 const TodoList = () => {
      const [isLoading, setIsLoading] = useState(false);
      const [addingTodo, setAddingTodo] = useState(false);
      const [todos, setTodos] = useState<Todo[]>([]);
      const [isSuccess, setIsSuccess] = useState(false);
+     const [inputValue, setInputValue] = useState("");
+     const [editableTodo, setEditableTodo] = useState(false);
+     const [editableTodoId, setEditableTodoId] = useState<number | null>(null);
      const inpRef = useRef<HTMLInputElement | null>(null);
-     const formRef = useRef<HTMLFormElement | null>(null);
 
      // ************ Add TODO ****************
-     async function addTodo(e: SyntheticEvent) {
-          e.preventDefault();
+     async function handleAddTodo() {
           const todoInfo: Todo = {
                completed: false,
                id: Math.floor(Math.random() * 1000),
-               title: formRef.current?.todo?.value,
+               title: inputValue,
                userId: Math.floor(Math.random() * 100),
           };
           try {
@@ -29,7 +31,7 @@ const TodoList = () => {
                setIsSuccess(true);
                setAddingTodo(false);
                toast.success("Successfully added!!");
-               formRef?.current?.reset();
+               setInputValue("");
           } catch (error) {
                toast.error("Something went wrong!!");
           } finally {
@@ -56,6 +58,40 @@ const TodoList = () => {
      const handleRemoveTodo = (id: number) => {
           const updatedTodos = todos.filter(todo => todo.id !== id);
           setTodos(updatedTodos);
+          toast.success("Todos removed successfully");
+     };
+
+     // ************ Handle Edit Todo ****************
+     const handleEditTodo = (id: number) => {
+          setEditableTodoId(id);
+
+          const findTodo = todos.find(todo => todo.id === id);
+          if (findTodo) {
+               setInputValue(findTodo.title);
+          }
+     };
+
+     // ************ Handle Update Todo ****************
+     const updateTodo = async () => {
+          setEditableTodo(true);
+          const updatedTodo = {
+               title: inputValue,
+               completed: false,
+          };
+          try {
+               const response = await axios.patch(`https://jsonplaceholder.typicode.com/todos/${editableTodoId}`, updatedTodo);
+               const data = (await response.data) as Todo;
+               const updatedTodos: Todo[] = todos.map(todo => (todo.id === data.id ? { ...todo, title: data.title } : todo));
+               setInputValue("");
+               setTodos(updatedTodos);
+               setEditableTodo(false);
+               setEditableTodoId(null);
+               toast.success("Todo Updated successfully");
+          } catch (error) {
+               toast.error("Something went wrong");
+          } finally {
+               setEditableTodo(false);
+          }
      };
 
      //  ************ Focus on input field ****************
@@ -85,14 +121,23 @@ const TodoList = () => {
                                              checked={todo.completed}
                                              onChange={() => handleComplete(todo.id)}
                                         />
-                                        <label htmlFor={`${todo.id}`} className={`text-lg ${todo.completed && "line-through opacity-75"}`}>
+                                        <label htmlFor={`${todo.id}`} className={`text-lg pr-10 ${todo.completed && "line-through opacity-75"}`}>
                                              {todo.title}
                                         </label>
-                                        <img
-                                             src={removeImg}
-                                             onClick={() => handleRemoveTodo(todo.id)}
-                                             className="w-6 absolute right-4 cursor-pointer"
-                                        />
+                                        <div className="absolute right-3 flex gap-3">
+                                             <img
+                                                  src={editImg}
+                                                  alt="Edit image"
+                                                  className="w-4 cursor-pointer"
+                                                  onClick={() => handleEditTodo(todo.id)}
+                                             />
+                                             <img
+                                                  src={removeImg}
+                                                  className="w-4 cursor-pointer "
+                                                  onClick={() => handleRemoveTodo(todo.id)}
+                                                  alt="Remove image"
+                                             />
+                                        </div>
                                    </div>
                               ))}
                          </div>
@@ -100,20 +145,28 @@ const TodoList = () => {
                </div>
 
                {/* ================= Input Form ============== */}
-               <footer className="absolute bottom-10 left-5 right-5">
-                    <form
-                         ref={formRef}
-                         onSubmit={addTodo}
+               <footer className="absolute bottom-10 left-0 right-0 lg:px-0 px-5">
+                    <div
+                         // onSubmit={addTodo}
                          className="flex items-center justify-between px-4 py-2 mt-8 bg-white border-2 border-purple-300 rounded-full shadow-xl w-full"
                     >
-                         <input ref={inpRef} type="text" name="todo" className="w-full px-3 py-2 bg-transparent focus:outline-none" />
                          <input
-                              type="submit"
-                              disabled={addingTodo}
-                              value="Add"
-                              className="px-8 py-1 text-lg font-semibold text-white duration-300 bg-purple-700 shadow-md cursor-pointer rounded-3xl hover:bg-purple-600 disabled:opacity-50 disabled:cursor-not-allowed"
+                              ref={inpRef}
+                              onChange={e => setInputValue(e.target.value)}
+                              type="text"
+                              value={inputValue}
+                              name="todo"
+                              className="w-full px-3 py-2 bg-transparent focus:outline-none"
                          />
-                    </form>
+                         <button
+                              type="submit"
+                              disabled={addingTodo || editableTodo}
+                              onClick={editableTodoId ? updateTodo : handleAddTodo}
+                              className="px-8 py-1 text-lg font-semibold text-white duration-300 bg-purple-700 shadow-md cursor-pointer rounded-3xl hover:bg-purple-600 disabled:opacity-50 disabled:cursor-not-allowed"
+                         >
+                              {editableTodoId ? "Update" : "Add"}
+                         </button>
+                    </div>
                </footer>
 
                {/* ============ Toaster =========== */}
