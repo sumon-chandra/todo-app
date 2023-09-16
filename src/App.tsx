d93ef6cb4689e3a8ff6e8226c59d979ffa0e8482
Sumon Chandra <topu199041@gmail.com>
2023-09-16 11:27:57 +0600

    Fetch todos and display them

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import TodoList from "./components/TodoList";
+
 function App() {
      return (
           <main className="w-full min-h-screen px-5 mx-auto lg:px-0 lg:w-5/12">
-               <div>
-                    <h3>Hello World</h3>
-               </div>
+               <header className="pt-8 text-3xl font-black text-center text-purple-700">
+                    <h3>TODO LIST APP</h3>
+               </header>
+               <section>
+                    <TodoList />
+               </section>
           </main>
      );
 }
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import { Todo } from "../types";
+import axios from "axios";
+import TodoCard from "./TodoCard";
+
+const TodoList = () => {
+     const [isLoading, setIsLoading] = useState(false);
+     const [todos, setTodos] = useState<Todo[]>([]);
+
+     async function fetchTodos() {
+          setIsLoading(true);
+          const response = await axios("https://jsonplaceholder.typicode.com/todos?_limit=10");
+          const data = await response.data;
+          setTodos(data);
+          setIsLoading(false);
+     }
+
+     useEffect(() => {
+          fetchTodos();
+     }, []);
+
+     return (
+          <div className="pt-10">
+               {isLoading ? (
+                    <p className="text-4xl font-black text-gray-400">Loading......</p>
+               ) : (
+                    <div className="flex flex-col gap-4">
+                         {todos.map(todo => (
+                              <TodoCard todo={todo} key={todo.id} />
+                         ))}
+                    </div>
+               )}
+          </div>
+     );
+};
+
+export default TodoList;
